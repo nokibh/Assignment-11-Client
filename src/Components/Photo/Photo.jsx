f19bbc9c9c1d044d68bda00845dabe4caa1325bc
nokibh <nokibhowlader2003@gmail.com>
2024-05-14 22:46:52 +0600

    added hover

--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -2,10 +2,16 @@ const Photo = ({ card }) => {
   const { food_image } = card;
   return (
     <div>
-      <div className="card card-side bg-base-100 shadow-xl dark:md:hover:bg-fuchsia-600 ">
-        <figure>
-          <img className="w-96 h-56" src={food_image} alt="Movie" />
-        </figure>
+      <div class="mb-4">
+        <div class="relative max-w-xs overflow-hidden bg-cover bg-no-repeat">
+          <img
+            className="w-96 h-56 rounded-lg"
+            src={food_image}
+            class="max-w-xs"
+            alt="Louvre"
+          />
+          <div class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-indigo-700 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-50"></div>
+        </div>
       </div>
     </div>
   );
